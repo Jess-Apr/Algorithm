104ca74d886f8ec0bbce1bb7b6b0b573d0e83473
Jess-Apr <jessie1944@naver.com>
2023-11-08 09:46:07 +0900

    231108 - [Bronze 5] - 단어 길이 재기

---
+++ Bronze/2743. 단어 길이 재기.js
@@ -0,0 +1,12 @@
+// 문제 번호: 2743번
+
+// 백준 제출용 코드
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
+const input = fs.readFileSync(filePath).toString().trim();
+
+function solution(input) {
+  return input.length;
+}
+
+console.log(solution(input));
