7a0a6d3f343df7650720d0e201426d01f04df096
Jess-Apr <jessie1944@naver.com>
2023-12-16 15:44:20 +0900

    231216 - [Silver 3] N과 M (5)

---
+++ Silver/15654. N과 M (5).js
@@ -0,0 +1,35 @@
+// 문제 번호: 15651번
+
+// 백준 제출용 코드
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+const [n, m] = input.shift().split(" ").map(Number);
+const arr = input[0].split(" ").map(Number);
+arr.sort((a, b) => a - b);
+
+function solution(n, m, arr) {
+  const answer = [];
+  const tmp = [];
+  const visited = Array(n).fill(0);
+
+  function dfs(len) {
+    if (len === m) {
+      answer.push(tmp.join(" "));
+    } else {
+      for (let i = 0; i < n; i++) {
+        if (visited[i]) continue;
+
+        tmp[len] = arr[i];
+        visited[i] = 1;
+        dfs(len + 1);
+        visited[i] = 0;
+      }
+    }
+  }
+
+  dfs(0);
+  return answer.join("\n");
+}
+
+console.log(solution(n, m, arr));
