eea19a6c51f9bfe6a6a9bfe71ba6feca9c325fe7
Jess-Apr <jessie1944@naver.com>
2023-12-20 11:00:00 +0900

    231220 - [Bronze 5] 나머지

---
+++ Bronze/10430. 나머지.js
@@ -0,0 +1,12 @@
+// 문제 번호: 10430번
+
+// 백준 제출용 코드
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
+const input = fs.readFileSync(filePath).toString().trim().split(" ");
+const [a, b, c] = input.map(Number);
+
+console.log((a + b) % c);
+console.log(((a % c) + (b % c)) % c);
+console.log((a * b) % c);
+console.log(((a % c) * (b % c)) % c);
