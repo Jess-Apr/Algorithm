e5ce942ecd08127d581e945af8f953640595bcb5
Jess-Apr <jessie1944@naver.com>
2024-01-06 20:34:43 +0900

    240106 - [Silver 2] 가장 긴 감소하는 부분 수열

---
+++ Silver/11722. 가장 긴 감소하는 부분 수열.js
@@ -0,0 +1,28 @@
+// 문제 번호: 11722번
+
+// 백준 제출용 코드
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+const n = +input.shift();
+const arr = input[0].split(" ").map(Number);
+
+function solution(n, arr) {
+  const dp = new Array(n).fill(0);
+
+  for (let i = 0; i < n; i++) {
+    let max = 0;
+
+    for (let j = 0; j <= i; j++) {
+      if (arr[j] > arr[i]) {
+        max = Math.max(dp[j], max);
+      }
+    }
+
+    dp[i] = max + 1;
+  }
+
+  return Math.max(...dp);
+}
+
+console.log(solution(n, arr));
