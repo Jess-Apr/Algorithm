2f6f751619c94a23fca4cb5b648743ffb909800b
Jess-Apr <jessie1944@naver.com>
2025-05-05 18:02:21 +0900

    LeetCode 폴더 생성, README 수정, 250505 - [Easy] Check if Array is Good

---
+++ LeetCode/Easy/2784. Check if Array is Good.js
@@ -0,0 +1,16 @@
+const input = require("./example");
+
+function isGood(nums) {
+  nums.sort((a, b) => a - b);
+
+  const n = nums.length - 1;
+
+  for (let i = 0; i < n; i++) {
+    if (nums[i] !== i + 1) return false;
+  }
+
+  return nums[n] === n;
+}
+
+const answer = isGood(input);
+console.log(answer);
---
+++ LeetCode/Easy/example.js
@@ -0,0 +1,1 @@
+module.exports = [3, 4, 4, 1, 2, 1];
