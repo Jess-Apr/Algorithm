c3eccc54dd3ec4445b8158e245ee305d5708b86b
Jess-Apr <jessie1944@naver.com>
2026-08-03 22:57:29 +0900

    260803 - [Lv.1] 삼총사

---
+++ Programmers/Lv.1/삼총사.js
@@ -0,0 +1,19 @@
+const input = require("./example");
+
+function solution(number) {
+  let answer = 0;
+  const len = number.length;
+
+  for (let i = 0; i < len - 2; i++) {
+    for (let j = i + 1; j < len - 1; j++) {
+      for (let k = j + 1; k < len; k++) {
+        if (number[i] + number[j] + number[k] === 0) answer++;
+      }
+    }
+  }
+
+  return answer;
+}
+
+const answer = solution(input);
+console.log(answer);
